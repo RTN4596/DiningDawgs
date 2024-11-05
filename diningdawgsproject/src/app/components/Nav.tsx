42c6b0dd4c3dc7df9c5b6ad4cdd2a868da236cd2
DaltonShepard <dcs19329@uga.edu>
2024-11-05 12:04:52 -0500

    Started Nav Bar

---
+++ diningdawgsproject/src/app/components/Nav.tsx
@@ -0,0 +1,9 @@
+import styles from './Nav.module.css';
+
+export default function Nav() { 
+    return (
+        <nav className={styles.nav}>
+            <h1 className={styles.h1}>Dining Dawgs Reviews</h1>
+        </nav>
+    );
+}
--- diningdawgsproject/src/app/page.tsx
+++ diningdawgsproject/src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import Nav from "./components/Nav"; 
 
 export default function Home() {
   return (
-      
+      <Nav />
   );
 }
