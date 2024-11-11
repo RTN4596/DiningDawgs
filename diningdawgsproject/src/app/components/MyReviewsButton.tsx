a88e1a73ba6da782be546e600126645280bb53d6
DaltonShepard <dcs19329@uga.edu>
2024-11-11 11:43:41 -0500

    added my-reviews file structure

--- diningdawgsproject/src/app/components/MyReviewsButton.tsx
+++ diningdawgsproject/src/app/components/MyReviewsButton.tsx
@@ -1,7 +1,13 @@
 import styles from './Button.module.css';
+import { useRouter } from 'next/router';
 
 export default function MyReviewsButton() {
+    const router = useRouter();
+
+    const myReviews = () => {
+        router.push('/my-reviews');
+    }
     return (
-            <button className={styles.button}>My Reviews</button>
+            <button onClick={myReviews} className={styles.button}>My Reviews</button>
     );
 }
