ddf436a9bf937379acb7d0a6701f354e11c5d462
RTN4596 <roberttnevin@gmail.com>
2024-11-11 16:13:45 -0500

    Added the 3 static items in My Reviews
    Added 3 items in the array that each contain an id, imageUrl, title, feedback, and a rating for the review. Used map to render each one onto the page. Just need to style them better now.

--- diningdawgsproject/src/app/my-reviews/page.tsx
+++ diningdawgsproject/src/app/my-reviews/page.tsx
@@ -2,13 +2,50 @@
 
 import Image from "next/image";
 import NavbarSignedIn from "../components/NavbarSignedIn";
+import BackButton from "../components/BackButton";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+    const reviews = [
+        {
+            id: 1,
+            imageLink: "/burger.jpg",
+            title: "Best Burger Ever",
+            feedback: "Burger was cooked just right and tasted amazing!",
+            rating: 5
+        },
+        {
+            id: 2,
+            imageLink: "/scrambledeggs.jpg",
+            title: "Solid Scrambled Eggs!",
+            feedback: "Eggs were soft and fluffy.",
+            rating: 4
+        },
+        {
+            id: 3,
+            imageLink: "/pasta.jpg",
+            title: "Disappointing Pasta",
+            feedback: "Pasta was very wet. Not to my liking.",
+            rating: 2
+        },
+    ];
+
     return (
         <div>
             <NavbarSignedIn />
+            <BackButton />
+            <h1 className="text-center text-5xl mt-6">Your Reviews</h1>
+            <div className="flex flex-col items-center p-4">
+                {reviews.map((review) => (
+                    <div key={review.id} className="bg-gray-300 mt-4 w-64">
+                        <Image src={review.imageLink} alt="Picture of related food item" width={100} height={100} />
+                        <h3>{review.title} {review.rating} / 5</h3>
+                        <p>{review.feedback}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 
