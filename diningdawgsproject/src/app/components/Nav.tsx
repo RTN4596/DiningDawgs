6b62152fb0ed7ebc36b8d840b74275b5512ed2c8
jShanks404 <james@j2c2.us>
2024-11-21 11:56:58 -0500

    Nav bar update
    Added the logo to the Nav bar and had to change small details in the css to make it look good.

--- diningdawgsproject/src/app/components/Nav.tsx
+++ diningdawgsproject/src/app/components/Nav.tsx
@@ -2,11 +2,15 @@
 import styles from './Nav.module.css';
 import SignInButton from './SignInButton';
 import Link from 'next/link';
+import Image from "next/image";
 
 export default function Nav() { 
     return (
         <nav className={styles.nav}>
-            <h1 className={styles.h1}>Dining Dawgs Reviews</h1>
+            <div className="flex items-center gap-2">
+                <h1 className={styles.h1}>Dining Dawgs Reviews</h1>
+                <img src="/diningdawgslogo.png" className="w-20 h-auto" alt="Dining Dawgs Logo" />
+            </div>
             <Link href="/login">
                 <SignInButton />
             </Link>
--- diningdawgsproject/src/app/components/NavbarSignedIn.tsx
+++ diningdawgsproject/src/app/components/NavbarSignedIn.tsx
@@ -12,8 +12,9 @@ export default function NavbarSignedIn() {
     }
     return (
         <nav className={styles.nav}>
-            <div className= {styles.div}>
-            <h1 className={styles.h1}>Dining Dawgs Reviews</h1>
+            <div className="flex items-center gap-2">
+                <h1 className={styles.h1}>Dining Dawgs Reviews</h1>
+                <img src="/diningdawgslogo.png" className="w-20 h-auto" alt="Dining Dawgs Logo" />
             </div>
             <div className={styles.div}>
             <MyReviewsButton />
