742d3682ca16bb7d2c5bce0b9f2f98faa0ce11db
RTN4596 <roberttnevin@gmail.com>
2024-11-21 11:36:10 -0500

    Added hashing for the password
    Used bcrypt to hash the password for more secure storage of user info.

--- diningdawgsproject/src/app/api/signup/route.ts
+++ diningdawgsproject/src/app/api/signup/route.ts
@@ -1,11 +1,18 @@
 import connectMongoDB from "../../libs/mongodb";
 import User from "@/app/models/user";
+import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
     const { username, email, password} = await request.json();
     await connectMongoDB();
-    const user = await User.create({ username, email, password });
-    return NextResponse.json(user);
+    const hashedPassword = await bcrypt.hash(password, 5)
+    const newUser = {
+        username,
+        password: hashedPassword,
+        email
+    }
+    await User.create(newUser);
+    return NextResponse.json(newUser);
 }
