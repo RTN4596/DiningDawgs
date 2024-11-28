1cd1d2e4086b485828cebdad0b0b23f1bc27e5bd
DaltonShepard <dcs19329@uga.edu>
2024-11-28 11:07:28 -0500

    Fixed error on my review page

--- diningdawgsproject/src/app/my-reviews/page.tsx
+++ diningdawgsproject/src/app/my-reviews/page.tsx
@@ -50,8 +50,8 @@ export default function Home() {
             <h1 className="text-center text-5xl mt-6">Reviews for {session?.user?.name || "User"}</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 justify-items-center">
             {reviews.map((review) => (
-                <div>
-                <div key={review.review_id} className="bg-gray-300 mt-5 w-80 p-4 shadow-lg shadow-red-500">
+                <div key={review.review_id}>
+                <div className="bg-gray-300 mt-5 w-80 p-4 shadow-lg shadow-red-500">
                 <h3>{review.title} {review.rating} / 5</h3>
                 <p>{review.description}</p>
                 <p>Dining Hall: {review.dining_hall}</p>
